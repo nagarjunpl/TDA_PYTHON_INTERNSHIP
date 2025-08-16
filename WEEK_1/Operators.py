f481aa49669307339e4bf0706a03730dc492ea8f
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-08-16 08:26:43 +0530

    Add files via upload

---
+++ WEEK_1/Operators.py
@@ -0,0 +1,63 @@
+# Arithmetic operator
+
+a = 10
+b = 20
+
+print(a + b) #addition
+print(a - b) #subtraction
+print(a * b) #multiplication
+print(a / b) #division
+print(a // b) #floor division
+print(a % b) #modulus
+print(a ** b) #exponentiation
+
+print(a+b*a/a*2//15)
+
+# Comparison operator
+a = 100
+
+a += 200 # 300 
+a -= 100 # 200
+a *= 2 # 400
+a /= 2 # 200
+a %= 2 # 0 
+
+
+# Common Arithmetic Operations
+
+a = 100
+b = 200
+
+print(a == b) # False
+print(a != b) # True
+print(a > b) # False
+print(a < b) # True
+print(a >= b) # False
+print(a <= b) # True
+
+# Logical Operator
+
+a = 100
+b = 200
+
+print(a and b) # 200
+print(a or b) # 100
+print(not a) # False
+print( 2>1 and 3<4) # True
+print( 2>1 and 3>4) # False
+
+
+# Membership Operator
+
+a = [1, 2, 3, 4, 5]
+
+print(1 in a) # True
+print(6 in a) # False
+print(1 not in a) # False
+print(6 not in a) # True
+
+b = "Hello"
+
+print("H" in b) # True
+print("h" in b) # False
+print("H" not in b) # False
